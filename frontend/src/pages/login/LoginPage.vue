829752749814064972e28713a9939e810e3ee22f
Afonso Lopes <2230994@my.ipleiria.pt>
2025-12-02 16:45:21 +0000

    Ligar o botão "Sign up" na página LoginPage.vue

--- frontend/src/pages/login/LoginPage.vue
+++ frontend/src/pages/login/LoginPage.vue
@@ -36,7 +36,7 @@
 
                 <div class="text-center text-sm">
                     <span class="text-gray-600">Don't have an account? </span>
-                    <a href="#" class="font-medium text-blue-600 hover:text-blue-500">
+                    <a href="/register" class="font-medium text-blue-600 hover:text-blue-500">
                         Sign up
                     </a>
                 </div>
